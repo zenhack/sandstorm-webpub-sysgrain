4d57bc273c5d43863035e6c07da200e58a98cd09
Ian Denhardt <ian@zenhack.net>
2020-04-14 00:16:52 -0400

    web_site_session: write some logic for content negotiation.

--- src/web_site_session.rs
+++ src/web_site_session.rs
@@ -1,3 +1,7 @@
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+};
 use sandstorm::{
     web_publishing_capnp::web_site,
     grain_capnp::ui_session,
@@ -24,7 +28,7 @@ impl web_session::Server for WebSessionImpl {
         let client = self.client.clone();
         Promise::from_future(async move {
             let params = params.get()?;
-            let _context = params.get_context()?;
+            let context = params.get_context()?;
             let ignore_body = params.get_ignore_body();
 
             let response = results.get();
@@ -36,21 +40,71 @@ impl web_session::Server for WebSessionImpl {
                 .get_request().send()
                 .promise.await?;
             let value = result.get()?.get_value()?;
-            if value.len() == 0 {
-                let mut client_error = response.init_client_error();
-                client_error.set_status_code(web_session::response::ClientErrorCode::NotFound);
-                // TODO: fill in the body.
-                Ok(())
-            } else {
-                let mut content = response.init_content();
-                content.set_status_code(web_session::response::SuccessCode::Ok);
-                content.set_mime_type("text/plain");
-                if !ignore_body {
-                    content.get_body().set_bytes(b"Test.");
+            match match_content(value,
+                                context.get_accept()?,
+                                context.get_accept_encoding()?) {
+                Some(ref entity) => {
+                    let mut content = response.init_content();
+                    content.set_status_code(web_session::response::SuccessCode::Ok);
+                    content.set_mime_type(entity.get_mime_type()?);
+                    if !ignore_body {
+                        // TODO: copy over the body properly.
+                        content.get_body().set_bytes(b"Test.");
+                    }
+                },
+                None => {
+                    let mut client_error = response.init_client_error();
+                    client_error.set_status_code(web_session::response::ClientErrorCode::NotFound);
+                    if !ignore_body {
+                        // TODO: get the response type & body from the web_site's not found handlers.
+                        client_error.set_description_html("404 Not found");
+                    }
                 }
-                Ok::<(), capnp::Error>(())
-            }
+            };
+            Ok(())
         })
     }
 }
 
+fn match_content<'a>(entities: capnp::struct_list::Reader<'a, web_site::entity::Owned>,
+                 accepted_types: capnp::struct_list::Reader<'a, web_session::accepted_type::Owned>,
+                 accepted_encodings: capnp::struct_list::Reader<'a, web_session::accepted_encoding::Owned>
+                 ) -> Option<web_site::entity::Reader<'a>> {
+
+    let mut accepted_types: Vec<_> = accepted_types.into_iter().collect();
+    accepted_types
+        .as_mut_slice()
+        .sort_by(|&l, &r| {
+            r.get_q_value().partial_cmp(&l.get_q_value()).unwrap_or(Ordering::Equal)
+        });
+
+    // TODO: Prioritize results by qValue; right now we just ignore it for
+    // Accept-Encoding.
+    let accepted_encodings: HashSet<_> = accepted_encodings
+        .into_iter()
+        .filter_map(|enc| {
+            enc.get_content_coding().map(|e| Some(e)).unwrap_or(None)
+        })
+        .collect();
+
+    let entities: HashMap<_, _> = entities.into_iter().filter_map(|entity| {
+        entity.get_mime_type()
+            .map(|mime_type| Some((mime_type, entity)))
+            .unwrap_or(None)
+    }).collect();
+
+    for typ in accepted_types {
+        if let Ok(mime_type) = typ.get_mime_type() {
+            if let Some(entity) = entities.get(mime_type)  {
+                let encoding_ok = entity
+                    .get_encoding()
+                    .map(|enc| accepted_encodings.contains(enc))
+                    .unwrap_or(false);
+                if encoding_ok {
+                    return Some(*entity);
+                }
+            }
+        }
+    }
+    return None
+}
