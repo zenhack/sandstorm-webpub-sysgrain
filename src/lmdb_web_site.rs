6819fd2f9bcc8662a663f6dcde9fb3334e9f9006
Ian Denhardt <ian@zenhack.net>
2020-04-21 23:50:10 -0400

    Remove duplicate entity_list module.

--- src/lmdb_web_site.rs
+++ src/lmdb_web_site.rs
@@ -1,3 +1,4 @@
+use crate::shortcuts::entity_list;
 use lmdb;
 use lmdb::Transaction;
 use std::{
@@ -71,13 +72,6 @@ impl web_site::Server for LMDBWebSite {
             Ok(())
         })
     }
-}
-
-
-mod entity_list {
-    use sandstorm::web_publishing_capnp::web_site;
-    pub type Owned = capnp::struct_list::Owned<web_site::entity::Owned>;
-    pub type Reader<'a> = capnp::struct_list::Reader<'a, web_site::entity::Owned>;
 }
 
 impl assignable::Server<entity_list::Owned> for EntitiesCell {
